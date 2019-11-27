1c9769a19ee40cd649dda1cfe2611345e493252a
Jatus <gianmarco.pettinato@gmail.com>
2019-11-27 19:45:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-[ -d "/github/workspace" ] && cd "/github/workspace"
+ls "/github/home"
+ls "/github/workspace"
+
 while IFS= read -r line
 do
     latexmk -pdf -interaction=nonstopmode ${line}/main.tex
