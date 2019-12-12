1a1aedaeee74beb56186d2cd446a38e212153d09
Gianmarco <gianmarco.pettinato@gmail.com>
2019-12-12 16:41:30 +0100

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,34 @@
 if [ "${INPUT_BUILD}" == true ]; then
   mkdir Documents
 fi
+
+declare -A failed
+i=0
 while IFS= read -r path
 do
     docName="${path%"${path##*[!/]}"}"
     docName="${docName##*/}"
     w_dir=${PWD}
-    cd $path && latexmk -pdf -interaction=nonstopmode main.tex
+    cd $path 
+    latexmk -pdf -interaction=nonstopmode main.tex
+    result=$?
+    if [ $result != 0 ]; then 
+	failed[i]=$docName
+	i=i+1
+    fi
     cd $w_dir;
     if [ "${INPUT_BUILD}" == true ]; then
-      mv $path/main.pdf ./Documents/$docName.pdf
+          mv $path/main.pdf ./Documents/$docName.pdf
     fi
 done < .fileToCompile
 
 if [ "${INPUT_BUILD}" == true ]; then
-  tar -czvf Documents.tar.gz Documents/*
+    tar -czvf Documents.tar.gz Documents/*
 fi
+
+if [ $i != 0 ]; then
+    echo -e "\n The following documents could not be produced:"
+    for x in "${failed[*]}"; do echo "- $x"; done
+    exit 1
+fi
+exit 0
